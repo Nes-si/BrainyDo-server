5507408804a7ebf6a5de8ccd9c566c476af18d43
Nes-si <golovin.nik@gmail.com>
2019-01-22 00:33:56 +0300

    Клауд-функции joinEvent, leaveEvent.

--- cloud/main.js
+++ cloud/main.js
@@ -118,6 +118,63 @@ Parse.Cloud.beforeSave(Parse.User, request => {
 });
 
 
+Parse.Cloud.define('joinEvent', async request => {
+  const {user} = request;
+  if (!user)
+    throw 'Must be signed in to call this Cloud Function.';
+
+  const {id} = request.params;
+  if (!id)
+    throw 'There is no id param!';
+
+  const event = await new Parse.Query('Event')
+    .get(id);
+
+  let members = event.get('members');
+  if (!members)
+    members = [];
+  if (members.indexOf(user) == -1) {
+    members.push(user);
+    event.set('members', members);
+    await event.save(null, {useMasterKey: true});
+    return true;
+  }
+
+  return false;
+});
+
+Parse.Cloud.define('leaveEvent', async request => {
+  const {user} = request;
+  if (!user)
+    throw 'Must be signed in to call this Cloud Function.';
+
+  const {id} = request.params;
+  if (!id)
+    throw 'There is no id param!';
+
+  const event = await new Parse.Query('Event')
+    .get(id);
+
+  const members = event.get('members');
+  if (members) {
+    let ind = -1;
+    for (let i = 0; i < members.length; i++) {
+      if (members[i].id == user.id) {
+        ind = i;
+        break;
+      }
+    }
+    if (ind != -1) {
+      members.splice(ind, 1);
+      event.set('members', members);
+      await event.save(null, {useMasterKey: true});
+      return true;
+    }
+  }
+  return false;
+});
+
+
 Parse.Cloud.define("checkPassword", request => {
   if (!request.user)
     throw 'Must be signed in to call this Cloud Function.';
